aa5dd8d748da56f7f9a3137e29a47f1d503380a5
FrancoEVP10 <vasquezemmanuel439@gmail.com>
2022-10-21 01:03:42 -0500

    feat(chat): implement repositories

---
+++ waw-chat-service/waw-chat-service/Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,11 @@
+﻿using waw_chat_service.Persistence.Context;
+
+namespace waw_chat_service.Persistence.Repositories;
+
+public class BaseRepository {
+    protected readonly AppDbContext context;
+
+    public BaseRepository(AppDbContext context) {
+        this.context = context;
+    }
+}
