424e62d90be021ba10d92fca0227d0c08c110cfb
FrancoEVP10 <vasquezemmanuel439@gmail.com>
2022-10-21 00:21:21 -0500

    feat: implemented chatroom and message domain models

--- waw-chat-service/waw-chat-service/Domain/Models/Message.cs
+++ waw-chat-service/waw-chat-service/Domain/Models/Message.cs
@@ -1,6 +1,13 @@
 ﻿namespace waw_chat_service.Domain.Models;
 
-public class Message
-{
-    
+public class Message : BaseModel {
+    public string Content { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+
+    //Relationships
+    public long SenderId { get; set; }
+    //public User? Sender { get; set; }
+
+    public long ChatRoomId { get; set; }
+    public ChatRoom? ChatRoom { get; set; }
 }
