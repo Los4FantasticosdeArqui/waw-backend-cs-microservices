8f0daf308de4e977cc60e29ce1e88f1b753841f0
FrancoEVP10 <vasquezemmanuel439@gmail.com>
2022-10-21 00:32:23 -0500

    feat(chat): implemented domain services communication

---
+++ waw-chat-service/waw-chat-service/Domain/Services/Communication/MessageResponse.cs
@@ -0,0 +1,8 @@
+﻿using waw_chat_service.Domain.Models;
+
+namespace waw_chat_service.Domain.Services.Communication;
+
+public class MessageResponse  : BaseResponse<Message>{
+    public MessageResponse(string message) : base(message) {}
+    public MessageResponse(Message resource) : base(resource) {}
+}
