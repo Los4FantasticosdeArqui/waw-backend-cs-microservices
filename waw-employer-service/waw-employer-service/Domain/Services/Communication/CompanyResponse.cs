a7fca67a27f4409e3fb208fb132084a670583827
Nicolas Hernandez <u201011264@upc.edu.pe>
2022-10-27 22:13:25 +0200

    feat: implement domain services communication

---
+++ waw-employer-service/waw-employer-service/Domain/Services/Communication/CompanyResponse.cs
@@ -0,0 +1,10 @@
+﻿using Models;
+
+namespace waw_employer_service.Domain.Services.Communication
+{
+    public class CompanyResponse : BaseResponse<Company>
+    {
+        public CompanyResponse(string message) : base(message) { }
+        public CompanyResponse(Company resource) : base(resource) { }
+    }
+}
---
+++ waw-employer-service/waw-employer-service/Domain/Services/Communication/ErrorResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace waw_employer_service.Domain.Services.Communication
+{
+    public class ErrorResponse
+    {
+        public string Message { get; }
+
+        public ErrorResponse(string message)
+        {
+            Message = message;
+        }
+    }
+}
