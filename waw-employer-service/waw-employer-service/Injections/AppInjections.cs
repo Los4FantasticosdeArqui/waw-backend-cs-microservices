ac98e63908d102a7bfef2e5ebddf36ea6718a046
Nicolas Hernandez <u201011264@upc.edu.pe>
2022-10-27 22:36:58 +0200

    feat: implement injections

---
+++ waw-employer-service/waw-employer-service/Injections/AppInjections.cs
@@ -0,0 +1,15 @@
+﻿using waw_employer_service.Domain.Repositories;
+using waw_employer_service.Persistence.Repositories;
+
+namespace waw_employer_service.Injections
+{
+    public static class AppInjections
+    {
+        public static void Register(IServiceCollection services)
+        {
+            CompanyInjections.Register(services);
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
---
+++ waw-employer-service/waw-employer-service/Injections/CompanyInjections.cs
@@ -0,0 +1,16 @@
+﻿using waw_employer_service.Domain.Repositories;
+using waw_employer_service.Domain.Services;
+using waw_employer_service.Persistence.Repositories;
+using waw_employer_service.Services;
+
+namespace waw_employer_service.Injections
+{
+    public static class CompanyInjections
+    {
+        public static void Register(IServiceCollection services)
+        {
+            services.AddScoped<ICompanyRepository, CompanyRepository>();
+            services.AddScoped<ICompanyService, CompanyService>();
+        }
+    }
+}
