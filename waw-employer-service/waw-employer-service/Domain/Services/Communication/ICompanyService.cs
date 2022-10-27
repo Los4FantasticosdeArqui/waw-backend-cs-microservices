16d8a5e696397ca7394c882e9b1a5e5c6f560385
Nicolas Hernandez <u201011264@upc.edu.pe>
2022-10-27 22:18:19 +0200

    feat: implement domain service interfaces

---
+++ waw-employer-service/waw-employer-service/Domain/Services/Communication/ICompanyService.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+
+namespace waw_employer_service.Domain.Services.Communication
+{
+    public interface ICompanyService
+    {
+        Task<IEnumerable<Company>> ListAll();
+        Task<CompanyResponse> Create(Company company);
+        Task<CompanyResponse> Update(long id, Company company);
+        Task<CompanyResponse> Delete(long id);
+    }
+
+}
