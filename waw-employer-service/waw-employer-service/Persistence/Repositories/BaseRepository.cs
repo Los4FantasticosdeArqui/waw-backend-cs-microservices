acc23d5ca52184c1fdc41a7b24e2cb71cc8998b3
Nicolas Hernandez <u201011264@upc.edu.pe>
2022-10-28 00:08:49 +0200

    feat: implement respositories

---
+++ waw-employer-service/waw-employer-service/Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using waw_employer_service.Persistence.Contexts;
+
+namespace waw_employer_service.Persistence.Repositories
+{
+    public class BaseRepository
+    {
+        protected readonly AppDbContext context;
+
+        public BaseRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+    }
+
+}
