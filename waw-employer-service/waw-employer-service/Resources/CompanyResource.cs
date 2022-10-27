bcfb42a048a87f139e0280791de572a1463e6f18
Nicolas Hernandez <u201011264@upc.edu.pe>
2022-10-27 23:46:43 +0200

    feat: implement job offer resources

---
+++ waw-employer-service/waw-employer-service/Resources/CompanyResource.cs
@@ -0,0 +1,19 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+
+namespace waw_employer_service.Resources
+{
+    public class CompanyResource
+    {
+        [SwaggerSchema("Company identifier", ReadOnly = true)]
+        public long Id { get; set; }
+
+        [SwaggerSchema("Company name", Nullable = false)]
+        public string Name { get; set; } = string.Empty;
+
+        [SwaggerSchema("Company address")]
+        public string? Address { get; set; }
+
+        [SwaggerSchema("Company email", Nullable = false)]
+        public string Email { get; set; } = string.Empty;
+    }
+}
