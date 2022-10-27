0ede8f0a99a6a174264f24411f3d3951d5d5fe82
Nicolas Hernandez <u201011264@upc.edu.pe>
2022-10-27 21:14:04 +0200

    feat: implement domain repositories

---
+++ waw-employer-service/waw-employer-service/Domain/Repositories/ICompanyRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace waw_employer_service.Domain.Repositories
+{
+    using Models;
+
+    public interface ICompanyRepository
+    {
+        Task<IEnumerable<Company>> ListAll();
+        Task Add(Company company);
+        Task<Company?> FindById(long id);
+        Task<Company?> FindByName(string name);
+        void Update(Company company);
+        void Remove(Company company);
+    }
+}
---
+++ waw-employer-service/waw-employer-service/Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace waw_employer_service.Domain.Repositories
+{
+    public class IUnitOfWork
+    {
+        Task Complete();
+    }
+}
