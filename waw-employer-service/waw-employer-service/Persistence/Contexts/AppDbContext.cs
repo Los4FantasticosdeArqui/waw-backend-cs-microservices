bea681c175daee0172764150ec3692fbbba3ca39
Nicolas Hernandez <u201011264@upc.edu.pe>
2022-10-27 23:40:04 +0200

    feat: implement AppDbContext

---
+++ waw-employer-service/waw-employer-service/Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using waw_employer_service.Domain.Model;
+using waw_employer_service.Extensions;
+
+namespace waw_employer_service.Persistence.Contexts
+{
+    public class AppDbContext : DbContext
+    {       
+        private DbSet<Company>? companies;       
+
+        public DbSet<Company> Companies
+        {
+            get => GetContext(companies);
+            set => companies = value;
+        }       
+
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);          
+
+            var companyEntity = builder.Entity<Company>();
+            companyEntity.ToTable("Companies");
+            companyEntity.HasKey(p => p.Id);
+            companyEntity.Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+            companyEntity.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            companyEntity.Property(p => p.Address).HasMaxLength(256);
+            companyEntity.Property(p => p.Email).IsRequired().HasMaxLength(256);
+
+            builder.UseSnakeCase();
+        }
+
+        private static T GetContext<T>(T? ctx)
+        {
+            if (ctx == null) throw new NullReferenceException();
+            return ctx;
+        }
+    }
+}
